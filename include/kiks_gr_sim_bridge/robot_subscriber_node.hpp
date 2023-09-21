bb794f5d694cccc224802e737add5c86552f1d5a
miya-tnct <ayim2612@gmail.com>
2023-09-21 20:55:22 +0900

    robot_subscriber_nodeを実装

--- include/kiks_gr_sim_bridge/robot_subscriber_node.hpp
+++ include/kiks_gr_sim_bridge/robot_subscriber_node.hpp
@@ -17,8 +17,75 @@
 #ifndef KIKS_GR_SIM_BRIDGE__ROBOT_SUBSCRIBER_NODE_HPP_
 #define KIKS_GR_SIM_BRIDGE__ROBOT_SUBSCRIBER_NODE_HPP_
 
+#include <chrono>
+#include <string>
+
+#include "geometry_msgs/msg/twist.hpp"
+#include "geometry_msgs/msg/pose_with_covariance_stamped.hpp"
+#include "pendulum_msgs/msg/joint_state.hpp"
+
+#include "kiks_gr_sim_bridge/ros_node_base.hpp"
+#include "grSim_Commands.pb.h"
+#include "grSim_Replacement.pb.h"
+
 namespace kiks::gr_sim_bridge
 {
+
+class RobotSubscriberNode : public RosNodeBase
+{
+public:
+  inline static std::string default_name() {return "gr_sim_bridge_robot_subuscriber";}
+
+  explicit RobotSubscriberNode(
+    grSim_Robot_Command * const command,
+    grSim_Replacement * const replacement,
+    bool team_is_yellow,
+    const rclcpp::NodeOptions & options = rclcpp::NodeOptions());
+
+  RobotSubscriberNode(
+    grSim_Robot_Command * const command, grSim_Replacement * const replacement,
+    bool team_is_yellow,
+    const std::string & node_name, const rclcpp::NodeOptions & options = rclcpp::NodeOptions());
+
+  RobotSubscriberNode(
+    grSim_Robot_Command * const command, grSim_Replacement * const replacement,
+    bool team_is_yellow,
+    const std::string & node_name, const std::string & node_namespace,
+    const rclcpp::NodeOptions & options = rclcpp::NodeOptions());
+
+  explicit RobotSubscriberNode(
+    grSim_Robot_Command * const command,
+    grSim_Replacement * const replacement,
+    bool team_is_yellow, rclcpp::Node::SharedPtr node);
+
+  void update_validity();
+
+private:
+  using TwistMsg = geometry_msgs::msg::Twist;
+  using JointMsg = pendulum_msgs::msg::JointState;
+  using PoseMsg = geometry_msgs::msg::PoseWithCovarianceStamped;
+
+  void subscribe_cmd_vel(TwistMsg::ConstSharedPtr cmd_vel_msg);
+
+  void subscribe_cmd_flat_kick(JointMsg::ConstSharedPtr cmd_flat_kick_msg);
+
+  void subscribe_cmd_chip_kick(JointMsg::ConstSharedPtr cmd_chip_kick_msg);
+
+  void subscribe_cmd_spinner(JointMsg::ConstSharedPtr cmd_spinner_msg);
+
+  template <bool kTeamIsYellow>
+  void subscribe_initialpose(PoseMsg::ConstSharedPtr initialpose_msg);
+
+  grSim_Robot_Command * const command_;
+  grSim_Replacement * const replacement_;
+  double chip_kick_coef_x_, chip_kick_coef_z_;
+  std::chrono::nanoseconds kick_valid_duration_;
+  rclcpp::Time kick_valid_time_;
+  rclcpp::Subscription<TwistMsg>::SharedPtr cmd_vel_subscription_;
+  rclcpp::Subscription<JointMsg>::SharedPtr cmd_flat_kick_subscription_,
+    cmd_chip_kick_subscription_, cmd_spinner_subscription_;
+  rclcpp::Subscription<PoseMsg>::SharedPtr initialpose_subscription_;
+};
 
 }  // namespace kiks::gr_sim_bridge
 
--- src/kiks_gr_sim_bridge/robot_subscriber_node.cpp
+++ src/kiks_gr_sim_bridge/robot_subscriber_node.cpp
@@ -16,7 +16,141 @@
 
 #include "kiks_gr_sim_bridge/robot_subscriber_node.hpp"
 
+#include <cmath>
+#include <cstddef>
+
 namespace kiks::gr_sim_bridge
 {
+
+RobotSubscriberNode::RobotSubscriberNode(
+  grSim_Robot_Command * const command,
+  grSim_Replacement * const replacement,
+  bool team_is_yellow,
+  const rclcpp::NodeOptions & options)
+: RobotSubscriberNode(command, replacement, team_is_yellow,
+    std::make_shared<rclcpp::Node>(this->default_name(), options))
+{
+}
+
+RobotSubscriberNode::RobotSubscriberNode(
+  grSim_Robot_Command * const command,
+  grSim_Replacement * const replacement,
+  bool team_is_yellow,
+  const std::string & node_name,
+  const rclcpp::NodeOptions & options)
+: RobotSubscriberNode(command, replacement, team_is_yellow, std::make_shared<rclcpp::Node>(node_name, options))
+{
+}
+
+RobotSubscriberNode::RobotSubscriberNode(
+  grSim_Robot_Command * const command, grSim_Replacement * const replacement,
+  bool team_is_yellow,
+  const std::string & node_name, const std::string & node_namespace,
+  const rclcpp::NodeOptions & options)
+: RobotSubscriberNode(command, replacement, team_is_yellow,
+    std::make_shared<rclcpp::Node>(node_name, node_namespace, options))
+{
+}
+
+RobotSubscriberNode::RobotSubscriberNode(
+  grSim_Robot_Command * const command,
+  grSim_Replacement * const replacement,
+  bool team_is_yellow,
+  rclcpp::Node::SharedPtr node)
+: RosNodeBase(std::move(node)),
+  command_(command),
+  replacement_(replacement)
+{
+  // Parameter of kick valid duration[s]
+  this->add_parameter<double>(
+    "kick_valid_duration", 0.5, [this](const auto & param) {
+      kick_valid_duration_ =
+      std::chrono::nanoseconds(static_cast<std::size_t>(param.as_double() * 1000 * 1000 * 1000));
+    });
+  // Parameter of chip kick angle[deg]
+  this->add_parameter<double>(
+    "chip_kick_deg", 60, [this](const auto & param) {
+      const auto angle = param.as_double() * (std::acos(-1.0) / 180.0);
+      chip_kick_coef_x_ = std::cos(angle);
+      chip_kick_coef_z_ = std::sin(angle);
+    });
+  // Initialize command
+  command_->set_kickspeedx(0);
+  command_->set_kickspeedz(0);
+  command_->set_veltangent(0);
+  command_->set_velnormal(0);
+  command_->set_velangular(0);
+  command_->set_spinner(false);
+  command_->set_wheelsspeed(false);
+  // Initialize subscriptions
+  cmd_vel_subscription_ = node_->create_subscription<TwistMsg>(
+    "cmd_vel",
+    this->get_dynamic_qos(),
+    std::bind(&RobotSubscriberNode::subscribe_cmd_vel, this, std::placeholders::_1));
+  cmd_flat_kick_subscription_ = node_->create_subscription<JointMsg>(
+    "cmd_flat_kick",
+    this->get_dynamic_qos(),
+    std::bind(&RobotSubscriberNode::subscribe_cmd_flat_kick, this, std::placeholders::_1));
+  cmd_chip_kick_subscription_ = node_->create_subscription<JointMsg>(
+    "cmd_chip_kick",
+    this->get_dynamic_qos(),
+    std::bind(&RobotSubscriberNode::subscribe_cmd_chip_kick, this, std::placeholders::_1));
+  cmd_spinner_subscription_ = node_->create_subscription<JointMsg>(
+    "cmd_spinner",
+    this->get_dynamic_qos(),
+    std::bind(&RobotSubscriberNode::subscribe_cmd_spinner, this, std::placeholders::_1));
+  initialpose_subscription_ = node_->create_subscription<PoseMsg>(
+    "initialpose",
+    this->get_dynamic_qos(),
+    std::bind(team_is_yellow ? &RobotSubscriberNode::subscribe_initialpose<true> : &RobotSubscriberNode::subscribe_initialpose<false>, this, std::placeholders::_1));
+}
+
+void RobotSubscriberNode::update_validity()
+{
+  if (node_->now() < kick_valid_time_) {
+    command_->set_kickspeedx(0);
+    command_->set_kickspeedz(0);
+  }
+}
+
+void RobotSubscriberNode::subscribe_cmd_vel(TwistMsg::ConstSharedPtr cmd_vel_msg)
+{
+  command_->set_veltangent(
+    cmd_vel_msg->linear.x == 0 ?
+    1 : (cmd_vel_msg->linear.y / cmd_vel_msg->linear.x));
+  command_->set_velnormal(std::hypot(cmd_vel_msg->linear.x, cmd_vel_msg->linear.y));
+  command_->set_velangular(cmd_vel_msg->angular.z);
+}
+
+void RobotSubscriberNode::subscribe_cmd_flat_kick(JointMsg::ConstSharedPtr cmd_flat_kick_msg)
+{
+  kick_valid_time_ = node_->now() + kick_valid_duration_;
+  command_->set_kickspeedx(cmd_flat_kick_msg->velocity);
+  command_->set_kickspeedz(0);
+}
+
+void RobotSubscriberNode::subscribe_cmd_chip_kick(JointMsg::ConstSharedPtr cmd_chip_kick_msg)
+{
+  kick_valid_time_ = node_->now() + kick_valid_duration_;
+  command_->set_kickspeedx(cmd_chip_kick_msg->velocity * chip_kick_coef_x_);
+  command_->set_kickspeedz(cmd_chip_kick_msg->velocity * chip_kick_coef_z_);
+}
+
+
+void RobotSubscriberNode::subscribe_cmd_spinner(JointMsg::ConstSharedPtr cmd_spinner_msg)
+{
+  command_->set_spinner(cmd_spinner_msg->velocity > 0);
+}
+
+template <bool kTeamIsYellow>
+void RobotSubscriberNode::subscribe_initialpose(PoseMsg::ConstSharedPtr initialpose_msg)
+{
+  auto robot = replacement_->add_robots();
+  const auto & pose = initialpose_msg->pose.pose;
+  robot->set_x(pose.position.x);
+  robot->set_y(pose.position.y);
+  robot->set_dir(std::atan2(pose.orientation.z, pose.orientation.w));
+  robot->set_yellowteam(kTeamIsYellow);
+}
 
 }  // namespace kiks::gr_sim_bridge

01d1f70afa1c4badcb884f6e00b0203096880152
miya-tnct <ayim2612@gmail.com>
2023-09-22 21:23:57 +0900

    ball_subscriber_nodeを実装

--- include/kiks_gr_sim_bridge/ball_subscriber_node.hpp
+++ include/kiks_gr_sim_bridge/ball_subscriber_node.hpp
@@ -17,8 +17,50 @@
 #ifndef KIKS_GR_SIM_BRIDGE__BALL_SUBSCRIBER_NODE_HPP_
 #define KIKS_GR_SIM_BRIDGE__BALL_SUBSCRIBER_NODE_HPP_
 
+#include <string>
+
+#include "geometry_msgs/msg/pose_with_covariance_stamped.hpp"
+
+#include "grSim_Replacement.pb.h"
+#include "kiks_gr_sim_bridge/ros_node_base.hpp"
+
 namespace kiks::gr_sim_bridge
 {
+
+class BallSubscriberNode : public RosNodeBase
+{
+public:
+  struct BallInfo
+  {
+    grSim_Replacement * replacement;
+  };
+
+  inline static std::string default_name() {return "gr_sim_bridge_ball_subuscriber";}
+
+  explicit BallSubscriberNode(
+    const BallInfo& robot_info,
+    const rclcpp::NodeOptions & options = rclcpp::NodeOptions());
+
+  BallSubscriberNode(
+    const BallInfo& robot_info,
+    const std::string & node_name, const rclcpp::NodeOptions & options = rclcpp::NodeOptions());
+
+  BallSubscriberNode(
+    const BallInfo& robot_info,
+    const std::string & node_name, const std::string & node_namespace,
+    const rclcpp::NodeOptions & options = rclcpp::NodeOptions());
+
+  explicit BallSubscriberNode(
+    const BallInfo& robot_info, rclcpp::Node::SharedPtr node);
+
+private:
+  using PoseMsg = geometry_msgs::msg::PoseWithCovarianceStamped;
+
+  void subscribe_initialpose(PoseMsg::ConstSharedPtr initialpose_msg);
+
+  const BallInfo ball_info_;
+  rclcpp::Subscription<PoseMsg>::SharedPtr initialpose_subscription_;
+};
 
 }  // namespace kiks::gr_sim_bridge
 
